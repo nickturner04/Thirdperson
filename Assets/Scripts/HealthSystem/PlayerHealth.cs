dc83ead47710d20e593922cd501ed72fb25afe01
Nick <nick.turner2016@outlook.com>
2022-10-28 20:17:53 +0100

    Removed some inheritance that didn't need to be there, separating the health system into two different components

--- Assets/Scripts/HealthSystem/PlayerHealth.cs
+++ Assets/Scripts/HealthSystem/PlayerHealth.cs
@@ -5,25 +5,31 @@ using UnityEngine;
 [RequireComponent(typeof(Health))]
 public class PlayerHealth : MonoBehaviour
 {
-    private HealthPlayer health;
+    private Health health;
     public GameManager gameManager;
     private GhostController ghostController;
     [SerializeField] private LabelManager labelManager;
 
+    public float resetTime = 20;
+    public float maxShield = 250;
+    public float shield;
+    private float currentResetTime = 0;
+    [SerializeField] private float regenSpeed = 1f;
+
+
     private void Awake()
     {
-        health = GetComponent<HealthPlayer>();
+        health = GetComponent<Health>();
         ghostController = GetComponent<GhostController>();
     }
 
     public void TakeDamage(float damage, Vector3 position)
     {
-        if ((health.shield > 0 && !ghostController.occupied) || health.godMode)
+        if ((shield > 0 && !ghostController.occupied) || health.godMode)
         {//If there is shield, take away damage from the shield and move the ghost to where the bullet hit.
             ghostController.Guard();
             ghostController.ghostActiveTimer = 0.1f;
-            ghostController.gTransform.position = transform.position + (position - transform.position).normalized;
-            ghostController.gTransform.rotation = Quaternion.LookRotation(position - transform.position);
+            ghostController.gTransform.SetPositionAndRotation(transform.position + (position - transform.position).normalized, Quaternion.LookRotation(position - transform.position));
         }
         if (health.godMode) return;
         health.TakeDamage(damage);
@@ -41,7 +47,16 @@ public class PlayerHealth : MonoBehaviour
     private void Update()
     {
         labelManager.SetHealth(health.health, health.maxHealth);
-        labelManager.SetShield(health.shield, health.maxShield);
+        labelManager.SetShield(shield, maxShield);
+
+        currentResetTime += Time.deltaTime;
+        if (currentResetTime >= resetTime)
+        {
+            //shield = Mathf.Lerp(shield, maxShield * 2, Time.deltaTime * regenSpeed);
+            shield += Time.deltaTime * regenSpeed;
+        }
+        if (shield > maxShield) shield = maxShield;
+        if (currentResetTime > resetTime) currentResetTime = resetTime;
     }
 
 }
